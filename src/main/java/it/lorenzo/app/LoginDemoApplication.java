8b2d2c3452c5a6fccc33561852ffcaf08854ef9e
lorenzo.mallardo <lorenzo.mallardo@accenture.com>
2019-01-17 23:16:22 +0100

    prova porta

--- src/main/java/it/lorenzo/app/LoginDemoApplication.java
+++ src/main/java/it/lorenzo/app/LoginDemoApplication.java
@@ -10,8 +10,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@PropertySources({ @PropertySource({ "classpath:application-repository.properties" }),
-		@PropertySource("classpath:application.properties") })
+//@PropertySources({ @PropertySource({ "classpath:application-repository.properties" }),
+//		@PropertySource("classpath:application.properties") })
 public class LoginDemoApplication {
 
 	public static void main(String[] args) {
