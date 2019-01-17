0987ef1ee433f39bf5bfd7eda54fceb9205a6561
lorenzo.mallardo <lorenzo.mallardo@accenture.com>
2019-01-17 14:15:14 +0100

    pulizia di codice

--- src/main/java/it/lorenzo/app/controller/IndexController.java
+++ src/main/java/it/lorenzo/app/controller/IndexController.java
@@ -1,15 +1,20 @@
 package it.lorenzo.app.controller;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.HashSet;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +33,16 @@ public class IndexController {
 	public AuthenticationManager authenticationManager;
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String homepage() {
+	public String homepage(Model model, HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		System.out.println(principal);
+		String usernameLogged = principal.getName();
+		model.addAttribute("username", usernameLogged);
 		return "hello";
 	}
 
 	@RequestMapping(value = "/register/signup", method = { RequestMethod.GET, RequestMethod.POST })
-	public String registerUser(@ModelAttribute("user") UserInfoBean user, ModelMap map) throws IOException {
+	public String registerUser(@ModelAttribute("user") UserInfoBean user) throws IOException {
 		try {
 			HashSet<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
 			UserInfoBean activeUserInfo = userRepository.findByUsernameAndEnabled(user.getUsername(), true);
@@ -48,7 +57,6 @@ public class IndexController {
 		} catch (Exception e) {
 			System.out.println(e.getMessage() + e);
 		}
-		map.addAttribute("user", user);
 		return "redirect:/hello";
 	}
 }
--- src/main/java/it/lorenzo/app/controller/RegistrazioneController.java
+++ src/main/java/it/lorenzo/app/controller/RegistrazioneController.java
@@ -1,19 +1,12 @@
 package it.lorenzo.app.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
-//@RequestMapping("/booking/barber")
 public class RegistrazioneController {
 
-	@Autowired
-	private RestTemplate restTemplate;
-
-	private String url = "http://localhost:8080/rest";
 
 	@RequestMapping(value = "/registrazione", method = RequestMethod.GET)
 	public String registerUser() {
