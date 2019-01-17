9bdccabc32a8ac3fe69c8e66bb6632d7a449fa17
lorenzo.mallardo <lorenzo.mallardo@accenture.com>
2019-01-17 23:39:44 +0100

    fix

--- src/main/java/it/lorenzo/app/LoginDemoApplication.java
+++ src/main/java/it/lorenzo/app/LoginDemoApplication.java
@@ -1,10 +1,9 @@
 package it.lorenzo.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +13,12 @@ import org.springframework.web.client.RestTemplate;
 //		@PropertySource("classpath:application.properties") })
 public class LoginDemoApplication {
 
+	@Value("${prova.variabile}")
+	static String prova;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LoginDemoApplication.class, args);
+		System.out.println("***************************************" + prova);
 	}
 
 	@Bean
